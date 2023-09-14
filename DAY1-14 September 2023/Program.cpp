77afa109e32c2244cca07a21af41c2f3ca0edffe
Sparsh Gupta <125153117+sparshgupta121@users.noreply.github.com>
2023-09-14 17:52:04 +0530

    Learn About the Various DataTypes and Variables

---
+++ DAY1-14 September 2023/Program.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    int a=5;
+    cout<<"The Interger is "<<a<<"\n"<<endl;
+
+    char ch='a';
+    cout<<"The Character is" <<ch<<endl<<endl;
+
+    bool flag=true;
+    cout<<"the value of Bool is "<<flag<<"\n\n";
+
+
+}
