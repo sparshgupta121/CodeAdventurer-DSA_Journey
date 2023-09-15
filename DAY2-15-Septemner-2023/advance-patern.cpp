b778322acb0f7e43772eea520cb24877ccd21cdd
Sparsh Gupta <125153117+sparshgupta121@users.noreply.github.com>
2023-09-15 23:11:03 +0530

    DAY 2 DONE ✅

---
+++ DAY2-15-Septemner-2023/advance-patern.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+int q;
+cout<<"Enter the Number of Rows: ";
+cin>>q;
+
+for(int i=0;i<q;i++)
+{
+    for(int w=0;w<i+1;w++){
+        cout<<w+1;
+    }    
+    cout<<endl;
+}
+
+}
