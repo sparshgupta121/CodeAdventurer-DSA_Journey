32f14826044c9481a3a6fe980e10a6dd64dffce0
sparshgupta121 <sparshdheeeraj@gmail.com>
2023-09-16 23:41:58 +0530

    DAY 3 DONE ✅

--- DAY3-16-September-2023/program.cpp
+++ DAY3-16-September-2023/program.cpp
@@ -5,22 +5,22 @@ int main()
 
 {
     
-// int n;
-// cout<<"Enter Value for n: ";
-// cin>>n;
+int n;
+cout<<"Enter Value for n: ";
+cin>>n;
 
-// for (int row=0;row<n;row++){
-//     for(int col=0;col<n;col++)
-//     {
-//         if(row==0 ||col==0||col==n-row-1)
-//             cout<<"*";
-//         else{
-//             cout<<" ";
-//                 }
-// }
-//     cout<<endl;
+for (int row=0;row<n;row++){
+    for(int col=0;col<n;col++)
+    {
+        if(row==0 ||col==0||col==n-row-1)
+            cout<<"*";
+        else{
+            cout<<" ";
+                }
+}
+    cout<<endl;
 
-//     }
+    }
         
 
 int num;
