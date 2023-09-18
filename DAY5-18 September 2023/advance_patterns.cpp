f3bee0f991bac2f4a62f828904c1255c5291310c
sparshgupta121 <sparshdheeeraj@gmail.com>
2023-09-19 00:38:09 +0530

    Starting with Day 5

--- DAY5-18 September 2023/advance_patterns.cpp
+++ DAY5-18 September 2023/advance_patterns.cpp
@@ -4,7 +4,27 @@ using namespace std;
 int main()
 {
 
+int n;
+cin>>n;
+for (int i=0;i<n;i++){
+    int col;
+    for ( col=0;col<i+1;col++){
+        int ans= col+1;
+        char ch=ans+'A'-1;
+        cout<<ch;
 
+    }
 
+    col=col-1;
+
+    for(;col>=1;col--){
+        int ans= col+1;
+        char ch=col+'A'-1;
+        cout<<ch;  }
+    cout<<endl; 
     
+}
+
+
+
 }
