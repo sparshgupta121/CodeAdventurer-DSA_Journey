7b518acc0e340c0d0eede9ab6d68af284cf8aab3
sparshgupta121 <sparshdheeeraj@gmail.com>
2024-02-22 00:32:36 +0530

    2d Arrays Vectors

--- 2D Array/2darray.cpp
+++ 2D Array/2darray.cpp
@@ -1,12 +1,123 @@
 #include<iostream>
 #include<vector>
+#include <limits.h>
+
 using namespace std;
+
+// void transpose(int arr[][3], int rows, int cols)
+// {
+
+//     for(int i=0;i<rows;i++){
+//         for(int j=i;j<cols;j++){
+
+//             swap(arr[i][j],arr[j][i]);
+//         }
+//     }
+
+// }
+
+// void printarr(int arr[][3],int rows,int cols){
+
+//     for(int i=0;i<rows;i++){
+//         for(int j=0;j<cols;j++){
+
+//             cout<<arr[i][j]<<" ";
+//         }
+//         cout<<endl;
+//     }
+
+// }
 
 int main(){
 
-vector<int> mkb;
-mkb.push_back(5);
-cout<<mkb[0];
+    // vector<int> mkb;
+    // mkb.push_back(5);
+    // cout<<mkb[0];
+
+
+    // int rows=3;
+
+    // int cols=3;
+
+    // int arr[3][3]={{2,8,6},{1,4,6},{5,7,84}};
+
+    // for(int i=0;i<3;i++){
+    //     for(int j=0;j<3;j++){
+    //     cout<<brr[i][j]<<" ";
+
+    //     }
+
+    //     cout<<endl;
+    // }
+
+    // cout<<"max number\n";
+
+    // int max=INT_MIN;
+
+    // for(int j=0;j<3;j++){
+    //     for(int i=0;i<3;i++){
+    //     if(arr[i][j]>max)
+    //     max=arr[i][j];
+
+    //     }
+
+    // }
+
+    // cout<<max<<endl;
+
+    // cout<<"min number\n";
+
+    // int min=INT_MAX;
+
+    // for(int j=0;j<3;j++){
+    //     for(int i=0;i<3;i++){
+    //     if(brr[i][j]<min)
+    //     min=brr[i][j];
+
+    //     }
+
+    // }
+    //     cout<<min<<endl;
+
+
+    // printarr(arr,rows,cols);
+    // cout<<endl;
+    // transpose(arr, rows, cols);
+    // cout<<"transporsed"<<endl;
+    // printarr(arr,rows,cols);
+
+vector<vector<int> >arr;
+
+vector<int>a{1,2,3};
+vector<int>b{4,5,6};
+vector<int>c{7,8,9};
+
+arr.push_back(a);
+
+arr.push_back(b);
+arr.push_back(c);
+
+for(int i=0;i<arr.size();i++){
+    for(int j=0;j<arr[i].size();j++){
+        cout<<arr[i][j]<<" ";
+
+    }
+    cout<<endl;
+}
+
+
+vector<vector<int> >arr1(5,vector<int>(5,-8));
+
+
+        cout<<arr1[2][3]<<" "<<endl;
+
+for(int i=0;i<arr1.size();i++){
+    for(int j=0;j<arr1[i].size();j++){
+        cout<<arr1[i][j]<<" ";
+
+    }
+    cout<<endl;
+}
 
 
 }
--- Atrray-Lec2.cpp
+++ Atrray-Lec2.cpp
@@ -16,174 +16,174 @@ int find(vector<int>arr){
 
 int main(){
 
-// vector<int>ar1r;
+vector<int>ar1r;
 
-// int ans=(sizeof(ar1r)/sizeof(int));
-// cout<<ar1r.size()<<endl;
-// cout<<ar1r.capacity()<<endl;
-// cout<<ans<<endl;
+int ans=(sizeof(ar1r)/sizeof(int));
+cout<<ar1r.size()<<endl;
+cout<<ar1r.capacity()<<endl;
+cout<<ans<<endl;
 
-// ar1r.push_back(5);
-// ar1r.push_back(7);
-// ar1r.pop_back();
+ar1r.push_back(5);
+ar1r.push_back(7);
+ar1r.pop_back();
 
-// for(int i=0;i<ar1r.size();i++)
-// cout<<ar1r[i]<<" ";
+for(int i=0;i<ar1r.size();i++)
+cout<<ar1r[i]<<" ";
 
-// cout<<endl;
+cout<<endl;
 
-// vector<int> brr(10,50);
+vector<int> brr(10,50);
 
-// brr.push_back(4);
-// brr.push_back(8);
+brr.push_back(4);
+brr.push_back(8);
 
 
 
-// for(int i=0;i<brr.size();i++)
-// cout<<brr[i]<<" ";
+for(int i=0;i<brr.size();i++)
+cout<<brr[i]<<" ";
 
-// cout<<endl;
+cout<<endl;
 
-// cout<<"The Size of B: "<<brr.size()<<endl;
+cout<<"The Size of B: "<<brr.size()<<endl;
 
-// cout<<"The capacity of B: "<<brr.capacity()<<endl;
+cout<<"The capacity of B: "<<brr.capacity()<<endl;
 
 
 
-// vector<int> crr{10,50,40,52,12,34,89,71};
+vector<int> crr{10,50,40,52,12,34,89,71};
 
-// crr.push_back(4);
-// crr.push_back(8);
+crr.push_back(4);
+crr.push_back(8);
 
-// for(int i=0;i<crr.size();i++)
-// cout<<crr[i]<<" ";
+for(int i=0;i<crr.size();i++)
+cout<<crr[i]<<" ";
 
-// cout<<endl;
+cout<<endl;
 
-// cout<<"The Size of B: "<<crr.size()<<endl;
-// cout<<"The capacity of B: "<<crr.capacity()<<endl;
+cout<<"The Size of B: "<<crr.size()<<endl;
+cout<<"The capacity of B: "<<crr.capacity()<<endl;
 
 
-// cout<<endl;
+cout<<endl;
 
 
-// //Unique Element in Array
+//Unique Element in Array
 
-// int n;
-// cout<<"enter the size of n:"<<endl;
-// cin>>n;
+int n;
+cout<<"enter the size of n:"<<endl;
+cin>>n;
 
-// vector<int>arr(n);
+vector<int>arr(n);
 
-// cout<<"enter element: "<<endl;
+cout<<"enter element: "<<endl;
 
-// for(int i=0;i<n;i++){
-//     cin>>arr[i];
+for(int i=0;i<n;i++){
+    cin>>arr[i];
 
-// }
+}
 
-// int uqelem=find(arr);
+int uqelem=find(arr);
 
-// cout<<"Unique elemnt:"<<uqelem<<endl<<endl;
+cout<<"Unique elemnt:"<<uqelem<<endl<<endl;
 
 
-// //  union of arrays
+//  union of arrays
 
 
-// int z;
-// cout<<"Enter no of element and elem in ar1: "<<endl;
-// cin>>z;
-// vector<int> ar1(z);
+int z;
+cout<<"Enter no of element and elem in ar1: "<<endl;
+cin>>z;
+vector<int> ar1(z);
 
-// for (int i=0;i<z;i++){
-//     cin>>ar1[i];
-// }
+for (int i=0;i<z;i++){
+    cin>>ar1[i];
+}
 
 
-// int m;
-// cout<<"Enter no of element and elem in ar2: "<<endl;
+int m;
+cout<<"Enter no of element and elem in ar2: "<<endl;
 
-// cin>>m;
-// vector<int> ar2(m);
+cin>>m;
+vector<int> ar2(m);
 
-// for (int i=0;i<m;i++){
-//     cin>>ar2[i];
-// }
+for (int i=0;i<m;i++){
+    cin>>ar2[i];
+}
 
-// vector<int>ans1;
+vector<int>ans1;
 
-// //pushing elem of arr 
+//pushing elem of arr 
 
-// for (int i=0;i<ar1.size();i++){
-//     ans1.push_back(ar1[i]);
-// }
+for (int i=0;i<ar1.size();i++){
+    ans1.push_back(ar1[i]);
+}
 
 
-// for (int i=0;i<ar2.size();i++){
-//     ans1.push_back(ar2[i]);
-// }
+for (int i=0;i<ar2.size();i++){
+    ans1.push_back(ar2[i]);
+}
 
-// cout<<"Final Array is:"<<endl;
+cout<<"Final Array is:"<<endl;
 
-// for(int i=0;i<ans1.size();i++){
-//     cout<<ans1[i]<<" ";
-// }
+for(int i=0;i<ans1.size();i++){
+    cout<<ans1[i]<<" ";
+}
 
-// cout<<endl;
+cout<<endl;
 
 
 
-// int z;
-// cout<<"Enter no of element and elem in ar1: "<<endl;
-// cin>>z;
-// vector<int> ar1(z);
+int z;
+cout<<"Enter no of element and elem in ar1: "<<endl;
+cin>>z;
+vector<int> ar1(z);
 
-// for (int i=0;i<z;i++){
-//     cin>>ar1[i];
-// }
+for (int i=0;i<z;i++){
+    cin>>ar1[i];
+}
 
 
-// int m;
-// cout<<"Enter no of element and elem in ar2: "<<endl;
+int m;
+cout<<"Enter no of element and elem in ar2: "<<endl;
 
-// cin>>m;
-// vector<int> ar2(m);
+cin>>m;
+vector<int> ar2(m);
 
-// vector<int> ans;
+vector<int> ans;
 
 
-// for (int i=0;i<m;i++){
-//     cin>>ar2[i];
-// }
+for (int i=0;i<m;i++){
+    cin>>ar2[i];
+}
 
-// for (int i=0;i<ar1.size();i++){
-//     int element=ar1[i];
-//     for (int j=0;j<ar2.size();j++){
-//         if (element==ar2[j]){
-//             ar2[j]=-1;
-//             ans.push_back(element);
-//             break;
-//         }
+for (int i=0;i<ar1.size();i++){
+    int element=ar1[i];
+    for (int j=0;j<ar2.size();j++){
+        if (element==ar2[j]){
+            ar2[j]=-1;
+            ans.push_back(element);
+            break;
+        }
 
-//     }
+    }
 
 
-// }
+}
 
-// cout<<"The Element In the intersection of arrays: ";
-// for(int i=0;i<ans.size();i++)
-// cout<<ans[i]<<" ";
+cout<<"The Element In the intersection of arrays: ";
+for(int i=0;i<ans.size();i++)
+cout<<ans[i]<<" ";
 
 
 
-// int z;
-// cout<<"Enter no of element and elem in ar1: "<<endl;
-// cin>>z;
-// vector<int> arr(z);
+int z;
+cout<<"Enter no of element and elem in ar1: "<<endl;
+cin>>z;
+vector<int> arr(z);
 
-// for (int i=0;i<z;i++){
-//     cin>>arr[i];
-// }
+for (int i=0;i<z;i++){
+    cin>>arr[i];
+}
 
 int sum;
 cout<<"Enter Valur of Sum: "<<endl;
